116e27d5a85eff23055ee32e9bdf3be6d4c92cb3
Ehsan <ehsanabbasi840@yahoo.com>
2023-05-07 19:02:23 +0430

    init project

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-list=["a","b","c","d"]
+list=[1,2,3,4,5,6]
+evenList=[]
+oddList=[]
+sum=0
+s=0
 for x in list:
-    print(x)
+    if(x%2==0):
+        evenList.append(x)
+    else:
+        oddList.append(x)
+    s+=x
+print('Total Average :' ,s/(len(list)))
+for x in evenList:
+    sum+=x
+print('Even Average :' ,(sum)/(len(evenList)))
+sum=0
+for x in oddList:
+    sum+=x
+print('Odd Average :' ,(sum)/(len(evenList)))
+
